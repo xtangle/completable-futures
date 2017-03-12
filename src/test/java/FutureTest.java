65d38d9c674eb8ab12748586c4b3a404eb6038d2
xtangle <jackyx5724@hotmail.com>
2017-03-12 19:31:55 -0400

    Added test cases for the Future interface

--- src/test/java/FutureTest.java
+++ src/test/java/FutureTest.java
@@ -1,58 +1,191 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("Duplicates")
 public class FutureTest {
 
     class Image {
         private String name;
+        private long loadTime;
 
-        Image(String name) {
+        Image(String name, long loadTime) {
             this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void setName(String name) {
-            this.name = name;
+            this.loadTime = loadTime;
         }
 
         void load() throws InterruptedException, IllegalArgumentException {
-            Thread.sleep((long) (Math.random() * 100) + 25 * name.charAt(0));
-            System.out.println(String.format("Loaded image: %s", this.name));
+            System.out.println(String.format("Started loading image: %s", this.name));
+            Thread.sleep(this.loadTime);
+            System.out.println(String.format("Finished loading image: %s", this.name));
         }
     }
 
-    private final List<Image> IMAGES = Arrays.asList(
-            new Image("Cat"),
-            new Image("Dog"),
-            new Image("Elephant"),
-            new Image("Fox")
-    );
+    private void loadAll(Consumer<List<Image>> loadMethod) {
+        long start = System.nanoTime();
+        loadMethod.accept(IMAGES);
+        long end = System.nanoTime();
+        long totalTime = (end - start) / 1_000_000;
+        System.out.println(String.format("Finished loading all images. Total time: %dms", totalTime));
+    }
+
+    private final List<Image> IMAGES = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        IMAGES.clear();
+        Arrays.stream(new Image[]{
+                new Image("Cat", 1000),
+                new Image("Dog", 1000),
+                new Image("Elephant", 1000),
+                new Image("Fox", 1000)
+        }).forEach(IMAGES::add);
+    }
 
     @Test
-    public void testSerialLoad() throws Exception {
-        for (Image image : IMAGES) {
+    public void testSync_LoadAll() {
+        loadAll(images ->
+                images.forEach(image -> {
+                    try {
+                        image.load();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                })
+        );
+    }
+
+    @Test
+    public void testAsync() throws InterruptedException {
+        Image image = new Image("Cat", 10000);
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Future<Void> future = executor.submit(new Callable<Void>() {
+            public Void call() throws Exception {
+                image.load();
+                return null;
+            }
+        });
+
+        assertFalse(future.isDone());
+
+        System.out.println("While image is loading, we can do other stuff...");
+        Thread.sleep(5000);
+
+        try {
+            System.out.println("Now waiting for image to finish loading...");
+            future.get();
+            assertTrue(future.isDone());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Done");
+    }
+
+    @Test
+    public void testCancel() throws InterruptedException {
+        Image image = new Image("Cat", 10000);
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Future<Void> future = executor.submit(() -> {
             image.load();
+            return null;
+        });
+
+        assertFalse(future.isDone());
+        assertFalse(future.isCancelled());
+
+        Thread.sleep(8000);
+
+        System.out.println("Cancelling task...");
+        future.cancel(true);
+
+        assertTrue(future.isDone());
+        assertTrue(future.isCancelled());
+
+        System.out.println("Done");
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void testException() throws InterruptedException {
+        Image image = new Image("Cat", -10000);
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Future<Void> future = executor.submit(() -> {
+            image.load();
+            return null;
+        });
+
+        Thread.sleep(5000);
+
+        try {
+            future.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
+
+        assertTrue(future.isDone());
+        assertFalse(future.isCancelled());
+
+        System.out.println("Done");
     }
 
     @Test
-    public void testGet() {
+    public void testTimeout() throws InterruptedException {
+        Image image = new Image("Cat", 9999999999999L);
+        ExecutorService executor = Executors.newCachedThreadPool();
 
+        Future<Void> future = executor.submit(() -> {
+            image.load();
+            return null;
+        });
+
+        try {
+            future.get(5, TimeUnit.SECONDS);
+        } catch (ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        assertFalse(future.isDone());
+        assertFalse(future.isCancelled());
+
+        System.out.println("Done");
     }
 
     @Test
-    public void testCancel() {
+    public void testAsync_LoadAll() {
+        ExecutorService executor = Executors.newCachedThreadPool();
 
-    }
+        loadAll(images -> {
+            List<Future<Void>> futures = new ArrayList<>();
 
-    @Test
-    public void testGet_WithTimeout() {
+            images.forEach(image -> {
+                Future<Void> future = executor.submit(() -> {
+                    image.load();
+                    return null;
+                });
+                futures.add(future);
+            });
 
+            futures.forEach(future -> {
+                try {
+                    future.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            });
+        });
     }
 
 }
